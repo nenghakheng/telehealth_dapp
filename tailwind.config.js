b98b4b019ad4fcdec933be338190bc7e2c7af25d
hakraoul <haknjr11@gmail.com>
2024-11-21 08:10:21 +0700

    close #2 refactor project structure

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Add paths to your React files
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Example: custom colors
+        primary: "#1D4ED8",
+        secondary: "#9333EA",
+      },
+    },
+  },
+  plugins: [],
+};
